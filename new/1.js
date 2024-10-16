46a725b6788c42d7239553618acfc7357d225a3a
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2024-10-16 13:02:01 +0600

    16th Oct 2024

---
+++ new/1.js
@@ -0,0 +1,10 @@
+      // Print some text with document.write
+      document.write("kfjjfjd<br>");
+
+      // Define a variable 'name'
+      let name = "Rafsan<br>";
+
+      // Print the variable 'name' below the previous line
+      document.write(name);
+
+      document.write('<span style="font-size: 24px;">Rafsan Swadhin</span><br>');
